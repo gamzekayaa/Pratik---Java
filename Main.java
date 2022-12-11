62348b531088541d4e4a43d7391effddff2a8323
gamzekayaa <116214043+gamzekayaa@users.noreply.github.com>
2022-12-11 22:48:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       // Değişkenlerimizi oluşturalım
+        Scanner input =  new Scanner(System.in);
+        int a,b,c;
+        double u,alan ;
+        System.out.print("Üçgenin 1. kenarı:");
+        a = input.nextInt();
+        System.out.print("Üçgenin 2. kenarı:");
+        b = input.nextInt();
+        System.out.print("Üçgenin 3. kenarı:");
+        c = input.nextInt();
+
+        u = (a+b+c)/2;
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println("Üçgenin Alanı:" +alan);
+
+
+
+    }
+}
